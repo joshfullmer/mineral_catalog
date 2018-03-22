ae876becaf2a7f20937217f6f9982c80867349a1
Josh Fullmer <joshfullmer@me.com>
2018-03-21 19:14:28 -0700

    Begin unittesting

--- minerals/tests.py
+++ minerals/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+import load_from_json
+from minerals.models import Mineral
+
+
+class MineralTestCase(TestCase):
+    def setUp(self):
+        load_from_json.load_json_setup()
+        load_from_json.load_json_to_db()
+
+    def test_database_creation(self):
+        minerals = Mineral.objects.all()
+        self.assertEqual(len(minerals), 874)
+        first_mineral = minerals.first()
+        self.assertEqual(first_mineral.id, 1)
